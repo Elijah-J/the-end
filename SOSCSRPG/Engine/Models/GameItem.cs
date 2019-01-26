374fef351432862867fece350b49f0cc53592425
Eli <elijukanovich@gmail.com>
2019-01-26 12:58:17 -0800

    Add items

---
+++ SOSCSRPG/Engine/Models/GameItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class GameItem // public since it will be used in different projects
+    {
+        public int ItemTypeID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Weight { get; set; }
+
+        public GameItem(int itemTypeID, string name, int price, int weight) 
+        {
+            ItemTypeID = itemTypeID;
+            Name = name;
+            Price = price;
+            Weight = weight;
+        }
+
+        // since the user will add to items, we will make a clone
+        public GameItem Clone()
+        {
+            return new GameItem(ItemTypeID, Name, Price, Weight);
+        }
+    }
+}
---
+++ SOSCSRPG/Engine/Models/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem // this is a subclass
+    {
+        public Weapon(int itemTypeID, string name, int price, int weight) : base(itemTypeID, name, price, weight)
+        {
+
+        }
+    }
+}
